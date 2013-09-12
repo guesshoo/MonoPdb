f3de89bc75ff3936891f6465af3f88a38278b99b
Noel Hoo <noel.hoo@citi.com>
2013-09-12 17:16:34 +0100

    initial commit

---
+++ src/PdbReader.fs
@@ -0,0 +1,21 @@
+﻿#light
+namespace FOO
+open Mono.Cecil
+open System
+open System.IO
+
+
+module PdbReader = 
+    
+    let toString bytes = Text.Encoding.UTF8.GetString bytes
+
+    let readPdbInfo dllPath = 
+        let m = ModuleDefinition.ReadModule(dllPath:string)
+        let header, bytes = m.GetDebugHeader()
+        let rsds = toString bytes.[0..3]
+        if "RSDS" <> rsds then
+            failwithf "unable to read pdb info from %s" dllPath
+        let guid = Guid bytes.[4..19]
+        //let n = BitConverter.ToUInt32(bytes, 20)
+        let fileName = (toString bytes.[24..]).TrimEnd [|(char)0uy|] |> Path.GetFileName
+        fileName, guid
---
+++ src/Script.fsx
@@ -0,0 +1,17 @@
+﻿//Reference: http://blog.ctaggart.com/2013/03/assembly-to-pdb-to-source-files.html
+
+let workingDir = sprintf @"%s\bin\debug" __SOURCE_DIRECTORY__
+System.Environment.CurrentDirectory <-  workingDir
+printfn "%s " workingDir;
+
+#I @"C:\workdir\3rdParty\MonoPdb\MonoPdb\MonoPdb\bin\debug"
+#r "Mono.Cecil.dll"
+#r "MonoPdb.dll"
+
+open FOO.PdbReader;;
+
+
+let dllPath = @"C:\WorkDir\git\simpliciti.configurationManager\fixtures\Simpliciti.ConfigurationManager.dll"
+
+let fileName, guid = readPdbInfo dllPath
+printfn "%s %A" fileName guid
